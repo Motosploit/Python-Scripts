029c3ffb63f67f482f67d5dc3262d7100228798f
motosploit <motosploit@gmail.com>
2017-05-11 09:43:07 -0400

    Updated hostname script for streamlining and add ip lookup script

--- IP_Lookup.py
+++ IP_Lookup.py
@@ -6,31 +6,29 @@
 #import socket module
 import socket
 import re
-# create get_hostname function
-def get_hostname(file):
+# create get_ip function
+def get_ip(file):
 # open file and read it, set to f
 	with open(file, 'r') as f:
 #loop through reading each line of f and set to line
 		for line in f:
-# lookup hostname of the ip from the read line
-# set the line to the variable hostname
-			hostname = socket.getfqdn(line.strip())
-			print hostname
-# remove ca.com domain from hostname
-			name_only = re.sub('(.com)','',hostname)
+# lookup ip of the hostname from the read line
+# set the line to the variable ip
+			ip = socket.gethostbyname(line.strip())
+#			print ip
 #append hostnames to file
 			e = open('output.xls','a')
-			e.write(name_only)
+			e.write(ip)
 			e.write("\n")
 			e.close()
-	#		print name_only
+
 # return the hostname and print it out
 	#return name_only
 
-# set ips.txt document to files variable
-files = 'ips.txt'
+# set hosts.txt document to files variable
+files = 'hosts.txt'
 #can i append it into the existing file?
 #call get_hostname function with files variable
-hosts = get_hostname(files)
+hosts = get_ip(files)
 #print hosts
 
